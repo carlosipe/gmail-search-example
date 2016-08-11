93b892f207c27f183b534821af0f19e7945c800e
CarlosIPe <carlos2@compendium.com.ar>
2016-08-12 00:30:15 +0200

    Changed specs, now filter emails instead of getting addresses

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require "mote"
 require "mote/render"
 require 'signet/oauth_2/client'
 require 'requests'
+require 'json'
+require 'open-uri'
 Cuba.plugin(Mote::Render)
 
 File.read(".env").scan(/(.*?)="?(.*)"?$/).each do |key, value|
@@ -16,7 +18,7 @@ class API
       :token_credential_uri =>  'https://www.googleapis.com/oauth2/v3/token',
       :client_id => ENV.fetch('GOOGLE_CLIENT_ID'),
       :client_secret => ENV.fetch('GOOGLE_CLIENT_SECRET'),
-      :scope => 'https://www.googleapis.com/auth/contacts.readonly',
+      :scope => 'https://www.googleapis.com/auth/gmail.readonly',
       :redirect_uri => ENV.fetch('REDIRECT_URI')
     ) 
   end
@@ -34,13 +36,27 @@ end
 
 Cuba.define do
   on param('code') do |code|
-    token = API.new.get_token(code)
-    url = 'https://www.google.com/m8/feeds/contacts/default/full?alt=json&v=3.0&access_token=' + token
-    require 'json'
-    render 'result', entry: JSON.parse(Requests.request(:get, url).body).fetch('feed').fetch('entry')
+    api  = API.new
+    token = api.get_token(code)
+    render 'form', token: token
   end
 
-  on root do  
+  on param('token'), param('words') do |token, words|
+    words = URI::encode(words)
+    url = "https://www.googleapis.com/gmail/v1/users/me/messages?alt=json&v=3.0&access_token=#{token}&q=#{words}"
+    result = JSON.parse(Requests.request(:get, url).body)
+    messages = result.fetch("messages")
+    first_message = messages.first
+    m = nil
+    if first_message
+      id = first_message.fetch("id")
+      url = "https://www.googleapis.com/gmail/v1/users/me/messages/#{id}?alt=json&v=3.0&access_token=#{token}&format=full"
+      m = JSON.parse(Requests.request(:get, url).body)
+    end
+    render 'result', result: m
+  end
+
+  on root do
     link = API.new.auth_link
     render 'index', link: link
   end
